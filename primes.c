247c85a88082ce9e1073bf0b7a7630453a6e75c8
Parth <41526125+ParthShroff@users.noreply.github.com>
2021-08-21 23:25:20 -0500

    Unique prime value per thread

---
+++ primes.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+void* routine(void* args){
+    int value = *(int*) args;
+    printf("%d ", primes[value]);
+    free(args);
+}
+
+int main(int argc, char* argv[]){
+    pthread_t th[10];
+    int i;
+    for(int i = 0; i < 10; i++){
+        int* a = (int*) malloc(sizeof(int));
+        *a = i;
+        if(pthread_create(&th[i], NULL, &routine, (void*) a)){ 
+            perror("Error in thread creation");
+        }
+    }
+    
+    for(int i = 0; i < 10; i++){
+        if(pthread_join(th[i], NULL)){
+            perror("Error in thread joining");
+        }
+    }
+}
