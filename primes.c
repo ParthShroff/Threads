645ef262eeac02e06c0c8bdb07fee9ff9afe0fe2
Parth <41526125+ParthShroff@users.noreply.github.com>
2021-08-21 23:29:56 -0500

    optimization to memory address being passed

--- primes.c
+++ primes.c
@@ -8,17 +8,14 @@ int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
 void* routine(void* args){
     int value = *(int*) args;
-    printf("%d ", primes[value]);
-    free(args);
+    printf("%d ", value);
 }
 
 int main(int argc, char* argv[]){
     pthread_t th[10];
     int i;
     for(int i = 0; i < 10; i++){
-        int* a = (int*) malloc(sizeof(int));
-        *a = i;
-        if(pthread_create(&th[i], NULL, &routine, (void*) a)){ 
+        if(pthread_create(&th[i], NULL, &routine, primes + i)){ 
             perror("Error in thread creation");
         }
     }
